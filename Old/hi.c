fce94117d2f7a59a4aad842ed6b18b69f59b4894
Harsh Amrute <harsh2amrute@gmail.com>
2025-01-28 21:34:38 +0530

    reducing space

--- Old/hi.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Hello Worldd\n");
-    return 0;
-}
--- Old/neew.java
+++
@@ -1,7 +0,0 @@
-public class neew
-{
-    public static void main(String[] Strings)
-    {
-        System.out.println("Hello World");
-    }
-}
